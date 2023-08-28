04e175679173d345c8fa1314475c2e824b2c91d5
邢贵志 <tsing@outlook.com>
2023-08-28 19:19:34 +0800

    with hello

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
+app.get("/hello", (req, res) => res.json({name: "Foo"}));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
